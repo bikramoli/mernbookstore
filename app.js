463766e93f5303031172b887fc132dde29512258
bikramoli <bikramoli75@gmail.com>
2022-01-05 00:16:52 +0545

    test api get

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const express = require ('express');
-const mongoose = require ('mongoose')
+const connectDB = require("./db");
 
+const books = require("./routes/api/books")
 const App = express();
 require("dotenv").config();
 
-const db = process.env.db;
-
-
-//db
-mongoose.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true 
-})
-.then(()=>{console.log("Database connetion successfull")})
-
+//db connection
+connectDB();
 
 App.get("/", (req, res)=>{
     res.send("hello world..!!!")
 })
+
+//routes
+App.use('/api/books', books) 
+
 const PORT = process.env.PORT || 3000;
 App.listen(PORT, ()=>{
     console.log(`App running on ${PORT}`)
