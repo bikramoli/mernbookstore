ce1d360782609ee2e12493283821519fdc40b380
bikramoli <bikramoli75@gmail.com>
2022-01-04 22:53:06 +0545

    app listning on port and send successful message

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ const App = express();
 App.get("/", (req, res)=>{
     res.send("hello world..!!!")
 })
+const PORT = process.env.PORT || 3000;
+App.listen(PORT, ()=>{
+    console.log(`App running on ${PORT}`)
+})
