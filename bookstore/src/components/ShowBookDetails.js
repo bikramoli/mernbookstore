59b1736627a048604178550b52113fa85c78cf69
bikramoli <bikramoli75@gmail.com>
2022-01-05 17:52:52 +0545

    bookItem table

--- bookstore/src/components/ShowBookDetails.js
+++ bookstore/src/components/ShowBookDetails.js
@@ -1,6 +1,51 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 function ShowBooksDetails(){
+    const book = this.state.book;
+    let BookItem = <div>
+      <table className="table table-hover table-dark">
+        {/* <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead> */}
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Title</td>
+            <td>{ book.title }</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Author</td>
+            <td>{ book.author }</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>ISBN</td>
+            <td>{ book.isbn }</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Publisher</td>
+            <td>{ book.publisher }</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Published Date</td>
+            <td>{ book.published_date }</td>
+          </tr>
+          <tr>
+            <th scope="row">6</th>
+            <td>Description</td>
+            <td>{ book.description }</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     return(
         <div className="ShowBookDetails">
         <div className="container">
