6ccbdc784f1e233649f1a56c4fa8eded0436370c
bikramoli <bikramoli75@gmail.com>
2022-01-05 14:19:22 +0545

    createbook page

---
+++ bookstore/src/components/CreateBooks.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function CreateBook(){
+    return(
+        <>
+          <div className="CreateBook">
+              <div className="container">
+                  <div className="row">
+                     <form>
+                         <input type='text' name="title" placeholder="Type title here.."/>
+                         <input type='text' name="title" placeholder="Type title here.."/>
+                         <input type='text' name="title" placeholder="Type title here.."/>
+                         <input type='text' name="title" placeholder="Type title here.."/>
+                         <input type='text' name="title" placeholder="Type title here.."/>
+                         <input type='text' name="title" placeholder="Type title here.."/>
+                         
+                     </form>
+                  </div>
+   
+              </div>
+         
+          </div>
+        </>
+    )
+}
+export default CreateBook;
---
+++ bookstore/src/components/ShowBookList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function CreateBook(){
+    return(
+        <>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+
+          <h1>Hello</h1>
+
+          
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          
+        </>
+    )
+}
+export default CreateBook;
