1bbda89773683e2d77c758cf4242c589ea8f390b
bikramoli <bikramoli75@gmail.com>
2022-01-05 17:49:47 +0545

    details of book, get single item

---
+++ bookstore/src/components/ShowBookDetails.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function ShowBooksDetails(){
+    return(
+        <div className="ShowBookDetails">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10 m-auto">
+              <br /> <br />
+              <Link to="/" className="btn btn-outline-warning float-left">
+                  Show Book List
+              </Link>
+            </div>
+            <br />
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Book's Record</h1>
+              <p className="lead text-center">
+                  View Book's Info
+              </p>
+              <hr /> <br />
+            </div>
+          </div>
+          <div>
+            { BookItem }
+          </div>
+
+          <div className="row">
+            <div className="col-md-6">
+              <button type="button" className="btn btn-outline-danger btn-lg btn-block" onClick={this.onDeleteClick.bind(this,book._id)}>Delete Book</button><br />
+            </div>
+
+            <div className="col-md-6">
+              <Link to={`/edit-book/${book._id}`} className="btn btn-outline-info btn-lg btn-block">
+                    Edit Book
+              </Link>
+              <br />
+            </div>
+
+          </div>
+            {/* <br />
+            <button type="button" class="btn btn-outline-info btn-lg btn-block">Edit Book</button>
+            <button type="button" class="btn btn-outline-danger btn-lg btn-block">Delete Book</button> */}
+
+        </div>
+      </div>
+
+    )
+}
+export default ShowBooksDetails;
