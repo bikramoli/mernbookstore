f9af6fd60484675708fc3162367b58c10c803f13
bikramoli <bikramoli75@gmail.com>
2022-01-05 14:23:07 +0545

    create book page

--- bookstore/src/components/CreateBooks.js
+++ bookstore/src/components/CreateBooks.js
@@ -1,25 +1,104 @@
 import React from "react";
 
 function CreateBook(){
+
+    const handleSubmit = () => {
+
+    }
     return(
         <>
-          <div className="CreateBook">
-              <div className="container">
-                  <div className="row">
-                     <form>
-                         <input type='text' name="title" placeholder="Type title here.."/>
-                         <input type='text' name="title" placeholder="Type title here.."/>
-                         <input type='text' name="title" placeholder="Type title here.."/>
-                         <input type='text' name="title" placeholder="Type title here.."/>
-                         <input type='text' name="title" placeholder="Type title here.."/>
-                         <input type='text' name="title" placeholder="Type title here.."/>
-                         
-                     </form>
-                  </div>
-   
-              </div>
-         
+         return (
+      <div className="CreateBook">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <br />
+              <Link to="/" className="btn btn-outline-warning float-left">
+                  Show BooK List
+              </Link>
+            </div>
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Add Book</h1>
+              <p className="lead text-center">
+                  Create new book
+              </p>
+
+              <form noValidate onSubmit={onSubmit}>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Title of the Book'
+                    name='title'
+                    className='form-control'
+                    value={state.title}
+                    onChange={onChange}
+                  />
+                </div>
+                <br />
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='ISBN'
+                    name='isbn'
+                    className='form-control'
+                    value={state.isbn}
+                    onChange={onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Author'
+                    name='author'
+                    className='form-control'
+                    value={state.author}
+                    onChange={onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Describe this book'
+                    name='description'
+                    className='form-control'
+                    value={state.description}
+                    onChange={onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='date'
+                    placeholder='published_date'
+                    name='published_date'
+                    className='form-control'
+                    value={state.published_date}
+                    onChange={onChange}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Publisher of this Book'
+                    name='publisher'
+                    className='form-control'
+                    value={state.publisher}
+                    onChange={onChange}
+                  />
+                </div>
+
+                <input
+                    type="submit"
+                    className="btn btn-outline-warning btn-block mt-4"
+                />
+              </form>
           </div>
+          </div>
+        </div>
+      </div>
         </>
     )
 }
