700caf255b5dd7782e8aba74c6a9a35608430b81
bikramoli <bikramoli75@gmail.com>
2022-01-06 13:37:13 +0545

    creating encryptPassword method, encrypt using crypto sha1 and salt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uuidv1 = require("uuidv1");
+
 
 
 const userSchema = new mongoose.Schema(
@@ -39,6 +39,25 @@ userSchema.virtual("password")
 .set(function(password){
     this._password = password
     this.salt = uuidv1()
+    this.hashed_password = this.encryptPassword(password)
 
 })
-.get()
+.get(function(){
+    return this._password
+})
+
+userSchema.method = {
+    encryptPassword: function(password){
+       if(!password){
+           return ""
+       }
+       try{
+          return crypto.createHmac('sha1', this.salt)
+                          .update(password)
+                          .digest('hex')
+
+       }catch(err){
+           return "";
+       }
+    }
+}
