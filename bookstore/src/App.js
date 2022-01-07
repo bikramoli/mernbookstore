7d4b74ed434fd74977d17af6549a53b1d76b8904
bikramoli <bikramoli75@gmail.com>
2022-01-07 14:10:06 +0545

    signup component

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -7,6 +7,7 @@ import ShowBookList from './components/ShowBookList';
 import ShowBookDetails from './components/ShowBookDetails';
 import UpdateBookInfo from './components/UpdateBookInfo';
 import Navbar from './components/Navbar';
+import SignUp from './components/SignUp';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <Navbar/>
         <Routes>
           <Route exact path='/' element={<ShowBookList/>} />
+          <Route path='/signup' element={<SignUp/>}/>
           <Route path='/create-book' element={<CreateBook/>} />
           <Route path='/edit-book/:id' element={<UpdateBookInfo/>} />
           <Route path='/show-book/:id' element={<ShowBookDetails/>} />
