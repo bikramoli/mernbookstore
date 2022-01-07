6153cb2867ba95c2dce01a4358a3f8e38280981a
bikramoli <bikramoli75@gmail.com>
2022-01-07 13:01:45 +0545

    persist token as t with expire date

--- controllers/user.js
+++ controllers/user.js
@@ -23,11 +23,13 @@ exports.signin = (req, res) => {
     User.findOne({email}, (err, user)=>{
             if(!user || err){
                 res.statue(400).json({err: "User doesnot exist"})
-            }
+            } 
             //if user found then check email and password match
             
 
             //generate signed token with the help of user id & secre
              const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
+             //persist token as "t" with expire date
+             res.cookie("t", token, {expire: new Date() +  9999})
     })
 }
