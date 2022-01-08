4e15eddb8cc634180292c8637547e2144256840a
bikramoli <bikramoli75@gmail.com>
2022-01-08 14:42:24 +0545

    user signout get request

--- bookstore/src/components/Navbar.js
+++ bookstore/src/components/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 
 function Navbar(){
+    const [name, setName] = useState(localStorage.getItem('user'))
     return(
         <>
           <div className='container'>
@@ -11,6 +12,7 @@ function Navbar(){
                       <li className='col-sm-2'>Add book</li>
                       <li className='col-sm-2'>Home</li>
                       <li className='col-sm-2'>Add book</li>
+                      <li className='col-sm-2'>{name}</li>
                   </ul>
               </div>
           </div>
